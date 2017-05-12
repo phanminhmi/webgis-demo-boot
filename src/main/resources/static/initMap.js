9fcbe0325daa5fc4bdb433a1d0b689a9dd22acce
Phan Minh Mi <mi.phan@axonactive.com>
2017-05-12 11:54:55 +0700

    Commit from intelliJ

--- src/main/resources/static/initMap.js
+++ src/main/resources/static/initMap.js
@@ -1,32 +1,41 @@
-$(document).ready(function initMap() {
+$(document).ready(
+    function initMap() {
         var uluru = {lat: parseFloat($("#lat").val()), lng: parseFloat($("#lon").val())};
         var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 4,
             center: uluru
         });
+        infoWindow = new google.maps.InfoWindow;
+
         var marker = new google.maps.Marker({
             position: uluru,
             map: map
         });
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Location found.');
+                infoWindow.open(map);
+                map.setCenter(pos);
+            }, function () {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    },
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
     }
 );
-
-$(document).ready(
-
-
-function eqfeed_callback(results) {
-    var heatmapData = [];
-    for (var i = 0; i < results.features.length; i++) {
-        var coords = results.features[i].geometry.coordinates;
-        var latLng = new google.maps.LatLng(coords[1], coords[0]);
-        heatmapData.push(latLng);
-    }
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-        data: heatmapData,
-        dissipating: false,
-        map: map
-    });
-}
-);
-
-
